b11a494570ac40b0be49230f890326efd0a9b55b
saddamhussain73 <164578060+saddamhussain73@users.noreply.github.com>
2024-11-10 15:53:43 +0500

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
+
+
+export default function about(){
+    return(
+
+  <div> 
+    <Navbar />
+    <br />
+    <h1>Hello world</h1>
+    <p>this is about page</p>
+    <br />
+    <Footer />
+    </div> 
+    )
+}
---
+++ src/app/contact_us/page.tsx
@@ -0,0 +1,19 @@
+
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
+
+
+export default function contact_us(){
+    return(
+  <div>
+   <Navbar /> 
+<br />
+<h1>Hello world</h1>
+<p>tis is contact us page</p>
+
+
+<br />
+<Footer />
+</div>  
+    )
+}
